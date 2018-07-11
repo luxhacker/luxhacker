1d3bc05aa685e1337293d2b64a284b1a49599bdf
luxing <lucianlux@163.com>
2018-07-11 09:56:08 +0800

    一元二次方程
    Co-Authored-By: luxing <luxing@users.noreply.github.com>

--- Demo20180710/quadratic.py
+++ Demo20180710/quadratic.py
@@ -10,4 +10,11 @@ def quadratic(a,b,c):
 
 	return x1,x2
 
-print('quadratic(2, 3, 1) =', quadratic(2, 3, 1))
+# print('quadratic(2, 3, 1) =', quadratic(2, 3, 1))
+
+if quadratic(2, 3, 1) != (-0.5, -1.0):
+    print('测试失败')
+elif quadratic(1, 3, -4) != (1.0, -4.0):
+    print('测试失败')
+else:
+    print('测试成功')
