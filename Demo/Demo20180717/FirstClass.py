25a251dcb5e0aca3bbd8175aeb642b03bf9692de
luxing <lucianlux@163.com>
2018-07-17 12:11:13 +0800

    luxhacker

---
+++ Demo/Demo20180717/FirstClass.py
@@ -0,0 +1,15 @@
+class Student(object):
+
+    def __init__(self, name,score):
+        self.name = name
+        self.score = score
+
+    
+    def print_score(self):
+        print('%s,%s' % (self.name,self.score))
+
+
+bart = Student('B','59')
+lisa = Student('C','61')
+bart.print_score()
+lisa.print_score()
